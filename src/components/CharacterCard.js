57e16194cfe8dea5cbe4b057feb1e3f654a904db
JW Nicholson <john.nicholson812@gmail.com>
2020-02-28 11:40:00 -0500

    returns and displays list of character names

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  const characterData = props.characterData;
+  return (
+    <>
+      {characterData.map(character => {
+        return (
+          <div className="characterWrapper">
+           
+            <h1>{character.name}</h1>
+            
+          </div>
+        );
+      })}
+    </>
+  );
 }
